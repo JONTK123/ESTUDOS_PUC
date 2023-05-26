000bc19589bd2b07fad62adf2cdc2f3936e28acf
Thiago Luiz Fossa <88333096+JONTK123@users.noreply.github.com>
2023-05-25 21:47:40 -0300

    Create FUNÇÕES.py

---
+++ RASCUNHOS E TREINOS copy/FUNÇÕES.py
@@ -0,0 +1,48 @@
+#Funções
+
+'''def nome(arg1...):
+    comandos
+
+    return Variavel/constante'''
+
+'''sem argumento
+
+   com valor padrao
+
+   "simples"
+
+   com valor padrao
+
+   nomeados'''
+
+# exemplo, função imprimir cabeçalho
+
+
+'''def cabeçalho():
+    
+     print('pipipi popopo')
+     print('pipipi popopo')
+     print('pipipi popopo')
+
+     return # vazio, nao irei usar novamente ( posso remover se eu quiser ) 
+
+cabeçalho() # chamando função
+
+# continuação do código.......'''
+
+def soma(a,b):
+
+    somadinha = a + b
+
+    return a,b
+
+variavel1 = input('digite o valor da variavel 1: ')
+
+variavel2 = input('digite o valor da variavel 2: ')
+
+resultado = soma(variavel1,variavel2)
+
+print(soma)
+
+
+
