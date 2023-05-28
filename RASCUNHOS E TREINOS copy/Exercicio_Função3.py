2813e5dc71b84ffdef0f0dd2845125ca1fb71c1f
Thiago Luiz Fossa <88333096+JONTK123@users.noreply.github.com>
2023-05-28 18:54:38 -0300

    Update Exercicio_Função3.py

--- RASCUNHOS E TREINOS copy/Exercicio_Função3.py
+++ RASCUNHOS E TREINOS copy/Exercicio_Função3.py
@@ -1,2 +1,17 @@
-def somaImposto(taxaImposto,custo):
-     
+'''Faça um programa com uma função chamada somaImposto. A 
+função possui dois parâmetros formais: taxaImposto, que é a quantia 
+de imposto sobre vendas expressa em porcentagem e custo, que é o 
+custo de um item antes do imposto. A função “altera” o valor de custo para incluir o imposto sobre vendas'''
+
+def somaImposto(taxaImposto, custo):
+
+    # taxaImposto = imposto / custo / 100
+    imposto = (taxaImposto * custo) / 100
+
+    
+
+
+
+
+
+    return
