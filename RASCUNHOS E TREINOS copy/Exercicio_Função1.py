8db403db5fc7f1e8fe4619e0c0ec0fcc6e6d8ca5
Thiago Luiz Fossa <88333096+JONTK123@users.noreply.github.com>
2023-05-28 20:41:57 -0300

    em

--- RASCUNHOS E TREINOS copy/Exercicio_Função1.py
+++ RASCUNHOS E TREINOS copy/Exercicio_Função1.py
@@ -1,21 +1,10 @@
-def imprimir_Antecessor_e_Sucessor(n):
-    Antecessor = n - 1
-    Sucessor = n + 1
-    print(f'Antecessor: {Antecessor} , Sucessor: {Sucessor}')
-    
-n = int(input("Digite um numero n: "))
-
-imprimir_Antecessor_e_Sucessor(n)
-
-def suce_ante(n):
-    sucessor = n + 1
-    antecessor = n - 1
-    print(f'Sucessor: {sucessor} Antecessor: {antecessor}')
+def suce_ante(y):
+    sucessor = y + 1
+    antecessor = y - 1
+    return sucessor, antecessor
 
 n = int(input('Digite um n: '))
-
-suce_ante(n) # BURRO NAO CHAMOU
-
-
+resultado = suce_ante(n)
+print(resultado)
 
 
--- RASCUNHOS E TREINOS copy/Exercicio_Função2.py
+++ RASCUNHOS E TREINOS copy/Exercicio_Função2.py
@@ -1,34 +1,32 @@
-def ler_numero(): # Def função, Nome função, () vazios pois nao precisa chamar
-    n = int(input("Digite o valor de um Numero inteiro: ")) 
-    return n
+# '''def ler_numero(): # Def função, Nome função, () vazios pois nao precisa chamar
+#     n = int(input("Digite o valor de um Numero inteiro: ")) 
+#     return n
 
-def somar(n1,n2,n3):
-    resultado = (n1 + n2 + n3)
-    return resultado
+# def somar(n1,n2,n3):
+#     resultado = (n1 + n2 + n3)
+#     return resultado
 
-n1 = ler_numero()
-n2 = ler_numero()
-n3 = ler_numero()
+# n1 = ler_numero()
+# n2 = ler_numero()
+# n3 = ler_numero()
 
-resp = somar(n1,n2,n3)
-print(resp)
+# resp = somar(n1,n2,n3)
+# print(resp)'''
 
 def ler():
     n = int(input('Digite um numero n: '))
     return n # Vou salvar como reusultado dessa funcao como n
 
-def soma(m1,m2,m3):
-    resultado = m1 + m2 + m3
+def soma(numero_inserido1, numero_inserido2, numero_inserido3):
+    resultado = numero_inserido1 + numero_inserido2 + numero_inserido3
+    print(resultado)
     return resultado # Vou salvar como reusultado dessa funcao com resultado
 
-m1 = ler()
-m2 = ler()
-m3 = ler()
+n1 = ler() # return do n do m1
+n2 = ler() # return do n do m2
+n3 = ler() # return do n do m3
 
-somatoria = soma(m1,m2,m3)
-print(somatoria)
-
+soma(n1,n2,n3)
 
 
 
-    
