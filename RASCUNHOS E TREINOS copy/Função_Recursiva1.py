31d8773fd38335cd65d33473618ab604cd5c6d31
Thiago Luiz Fossa <88333096+JONTK123@users.noreply.github.com>
2023-05-30 10:15:30 -0300

    .

--- RASCUNHOS E TREINOS copy/Função_Recursiva1.py
+++ RASCUNHOS E TREINOS copy/Função_Recursiva1.py
@@ -1,7 +1,7 @@
 # Calcular o fatorial de um número:
 
 def fatorial(n):
-    return n * ( n - 1)
+    return n * ( n - 1) # enquanto o n nao é zero ele nao para
 
 n = int(input('n: '))
 resultado = fatorial(n)
