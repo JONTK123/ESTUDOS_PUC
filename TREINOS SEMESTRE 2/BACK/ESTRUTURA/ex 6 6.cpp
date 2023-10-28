b34cbd75bcf24e03ba573226a0d7e9c513d8ce7f
Thiago Luiz Fossa <88333096+JONTK123@users.noreply.github.com>
2023-10-28 13:38:49 -0300

    a

--- TREINOS SEMESTRE 2/BACK/ESTRUTURA/ex 6 6.cpp
+++ TREINOS SEMESTRE 2/BACK/ESTRUTURA/ex 6 6.cpp
@@ -26,9 +26,44 @@ no *novo_no (){
     return novo;
 }
 
+void inserir_inicio(no**pri){ // Recebe o ponteiro do ponteiro de pri, que aponta para o primeiro ponteiro. Usaremos ele para udar seu valor sem mudar seu endereço definitivo 
+
+    no*novo=novo_no(); // criação de um novo nó
+
+    cout << "Digite o valor do elemento: "; // Elemento que será inserido no nó
+    cin >>novo->info; // Aloca para o campo info da estrutura nó
+
+    novo->link=*pri; // Aponta ponteiro link para o poteiro de *pri. Ponteiro de pri carrega as informações do primeiro nó
+    *pri = novo; // Agora o NOVO nó se torna o primeiro para continuar as ligações
+}
+
+void inserir_info(no**pri, elemento x, elemento v){
+    no *p = *pri;
+    no *novo = novo_no();
+
+
+    while (p != NULL){
+        if(p->info == x){
+            novo->info=v;
+            novo->link = p->link;
+            p->link=novo;
+            break;
+        }
+    }
+    p = p->link;
+}
+
 int main() {
     no * pri;
     pri = init();
+    int x = 5;
+    int v = 10;
+
+    for(int i = 0;i<5;i++){
+        inserir_inicio(&pri);
+    }
+
+    inserir_info(&pri, x, v);
 
     return 0;
 }
--- TREINOS SEMESTRE 2/BACK/ESTRUTURA/ex 7 7.cpp
+++ TREINOS SEMESTRE 2/BACK/ESTRUTURA/ex 7 7.cpp
@@ -23,9 +23,47 @@ no *novo_no (){
     return novo;
 }
 
+void inserir_inicio(no**pri){ // Recebe o ponteiro do ponteiro de pri, que aponta para o primeiro ponteiro. Usaremos ele para udar seu valor sem mudar seu endereço definitivo 
+
+    no*novo=novo_no(); // criação de um novo nó
+
+    cout << "Digite o valor do elemento: "; // Elemento que será inserido no nó
+    cin >>novo->info; // Aloca para o campo info da estrutura nó
+
+    novo->link=*pri; // Aponta ponteiro link para o poteiro de *pri. Ponteiro de pri carrega as informações do primeiro nó
+    *pri = novo; // Agora o NOVO nó se torna o primeiro para continuar as ligações
+}
+
+void elimina_no(no **pri){
+    no *p;
+    p = *pri;
+    no *anterior;
+
+    if (p == NULL ){
+        cout << "Lista está vazia";
+        return;
+    }
+
+    while (p != NULL) {
+        if (p -> link == NULL){
+        anterior = p;
+        p = p->link;
+        }
+    }
+
+    anterior->link = NULL;  
+
+}
+
 int main() {
     no * pri;
     pri = init();
+
+    for(int i = 0;i<5;i++){
+        inserir_inicio(&pri);
+    }
+
+    elimina_no(&pri);
 
     return 0;
 }
