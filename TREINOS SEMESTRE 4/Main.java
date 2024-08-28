b43327cf41feeebb0226af5ffcaf1bd51521afa3
Thiago Luiz Fossa <thiago.fossa433@gmail.com>
2024-08-28 10:09:12 -0300

    .

--- TREINOS SEMESTRE 4/Main.java
+++ TREINOS SEMESTRE 4/Main.java
@@ -75,32 +75,18 @@ class Horario {
 
         for (int i = 0; i < qtdSegundos; i++) {
             if (isValida(this.h, this.m, (byte) (this.s + 1))) {
-                if(this.s == 59){
-                    this.s = 0;
-                    this.m++;
-                } else {
-                    this.s++;
-                }
+                this.s++;
             } else if (isValida(this.h, (byte) (this.m + 1), this.s)) {
-                if (this.m == 59) {
-                    this.m = 0;
-                    this.h++;
-                } else {
-                    this.m++;
-                }
+                this.m++;
+                this.s = 0;
             } else if (isValida((byte) (this.h + 1), this.m, this.s)) {
-                if(this.h == 23) {
-                    this.h = 0;
-                    this.m = 0;
-                    this.s = 0;
-                } else {
                 this.h++;
                 this.m = 0;
                 this.s = 0;
-                }
-
-            } else {
-                throw new Exception("Horário inválido");
+            } else { // Mudança de dia 23:59:59
+                this.h = 0;
+                this.m = 0;
+                this.s = 0;
             }
         }
     }
@@ -113,18 +99,23 @@ class Horario {
         for (int i = 0; i < qtdSegundos; i++) {
             if (isValida(this.h, this.m, (byte) (this.s - 1))) {
                 this.s--;
-            } else if (isValida(this.h, (byte) (this.m - 1), this.s)) {
+            } else if (isValida(this.h, (byte) (this.m - 1), (byte) 59)) {
                 this.m--;
-            } else if (isValida((byte) (this.h - 1), this.m, this.s)) {
+                this.s = 59;
+            } else if (isValida((byte) (this.h - 1), (byte) 59, (byte) 59)) {
                 this.h--;
-            } else {
-                throw new Exception("Horário inválido");
+                this.m = 59;
+                this.s = 59;
+            } else { // Mudança de dia
+                this.h = 23;
+                this.m = 59;
+                this.s = 59;
             }
         }
     }
 
     public int diferenca(Horario NovaInstancia) throws Exception {
-        if (NovaInstancia == null) throw new Exception("Horario Invalido");
+        if (NovaInstancia == null) throw new Exception("Horário inválido");
 
         int thisSegundos = this.h * 3600 + this.m * 60 + this.s;
         int hSegundos = NovaInstancia.h * 3600 + NovaInstancia.m * 60 + NovaInstancia.s;
@@ -136,27 +127,45 @@ class Horario {
 public class Main {
     public static void main(String[] args) {
         try {
+            // Caso 1: Horário inicial
             Horario h1 = new Horario((byte) 10, (byte) 30, (byte) 40);
-            Horario h2 = new Horario((byte) 5, (byte) 2, (byte) 50);
+            System.out.println("Caso 1: " + h1.toString()); // Esperado: 10:30:40
 
-            System.out.println(h1.toString());
-            System.out.println(h2.toString());
-
+            // Caso 2: Adiantando 10 segundos
             h1.adiante(10);
-            System.out.println(h1.toString());
+            System.out.println("Caso 2: " + h1.toString()); // Esperado: 10:30:50
 
+            // Caso 3: Adiantando 20 segundos
             h1.adiante(20);
-            System.out.println(h1.toString());
+            System.out.println("Caso 3: " + h1.toString()); // Esperado: 10:31:10
 
+            // Caso 4: Adiantando 30 segundos
             h1.adiante(30);
-            System.out.println(h1.toString());
-            
-            System.out.println(h1.equals(h2));
+            System.out.println("Caso 4: " + h1.toString()); // Esperado: 10:31:40
 
+            // Caso 5: Atrasando 5 segundos
             h1.atrase(5);
-            System.out.println(h1.toString());
+            System.out.println("Caso 5: " + h1.toString()); // Esperado: 10:31:35
 
-            System.out.println(h1.diferenca(h2));
+            // Caso 6: Criando outro horário
+            Horario h2 = new Horario((byte) 5, (byte) 2, (byte) 50);
+            System.out.println("Caso 6: " + h2.toString()); // Esperado: 05:02:50
+
+            // Caso 7: Verificando igualdade
+            System.out.println("Caso 7: " + h1.equals(h2)); // Esperado: false
+
+            // Caso 8: Diferença entre horários
+            System.out.println("Caso 8: " + h1.diferenca(h2)); // Esperado: diferença em segundos
+
+            // Caso 9: Adiantando para mudança de dia
+            Horario h3 = new Horario((byte) 23, (byte) 59, (byte) 50);
+            h3.adiante(10);
+            System.out.println("Caso 9: " + h3.toString()); // Esperado: 00:00:00
+
+            // Caso 10: Atrasando para mudança de dia
+            Horario h4 = new Horario((byte) 0, (byte) 0, (byte) 10);
+            h4.atrase(20);
+            System.out.println("Caso 10: " + h4.toString()); // Esperado: 23:59:50
 
         } catch (Exception e) {
             System.out.println(e.getMessage());
