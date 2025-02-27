eaab3aad9ec46887174953300747e6428ead8434
Thiago Luiz Fossa <thiago.fossa433@gmail.com>
2025-02-27 16:54:47 -0300

    .

--- TREINOS SEMESTRE 5/tutorial/tutorial/spiders/brands_spider.py
+++ TREINOS SEMESTRE 5/tutorial/tutorial/spiders/brands_spider.py
@@ -1,13 +1,25 @@
 import scrapy
+from pymongo import MongoClient
+
+MONGO_URI = "mongodb+srv://thiagofossa433:GdKliEuCmoVBs2Z0@aulasbd.p8nc1.mongodb.net/"
+DATABASE_NAME = "Scrapy"
+COLLECTION_DATA = "Data"
+
+client = MongoClient(MONGO_URI)
+db = client[DATABASE_NAME]
 
 class BrandsSpider(scrapy.Spider):
     name = "brands"
     start_urls = [
         "https://www.rankingthebrands.com/The-Brands-and-their-Rankings.aspx",
     ]
+    all_brands = []
 
     def parse(self, response):
+        data_to_insert = []
         for brand in response.css("div#ctl00_mainContent_brandPanel div.brandLine"):
+            data_to_insert.append({"Brand": brand.css("span.rankingName::text").get()})
+            self.all_brands.append(brand.css("span.rankingName::text").get())
             yield {
                 "Brand": brand.css("span.rankingName::text").get()
             }
@@ -17,31 +29,8 @@ class BrandsSpider(scrapy.Spider):
         for letter_link in next_letters:
             yield response.follow(letter_link, callback=self.parse)
 
-# Ordem alfabetica
-#
-# import scrapy
-#
-# class BrandsSpider(scrapy.Spider):
-#     name = "brands"
-#     start_urls = [
-#         "https://www.rankingthebrands.com/The-Brands-and-their-Rankings.aspx",
-#     ]
-#
-#     def parse(self, response):
-#         # Coleta os links das letras (A-Z) e ordena para seguir em ordem alfabética
-#         next_letters = sorted(response.css("div#ctl00_mainContent_filterPanel a::attr(href)").getall())
-#
-#         for letter_link in next_letters:
-#             yield response.follow(letter_link, callback=self.parse_letter)
-#
-#     def parse_letter(self, response):
-#         # Extrai os nomes das marcas da página atual, seguindo a ordem de cada letra
-#         for brand in response.css("div#ctl00_mainContent_brandPanel div.brandLine"):
-#             yield {
-#                 "Brand": brand.css("span.rankingName::text").get()
-#             }
-#
-#         # Seguir para a próxima página dentro da mesma letra, se existir
-#         next_page = response.css("li.next a::attr(href)").get()
-#         if next_page:
-#             yield response.follow(next_page, callback=self.parse_letter)
+        # db[COLLECTION_DATA].insert_many(data_to_insert)
+
+    def closed(self, reason):
+        if self.all_brands:
+            db[COLLECTION_DATA].insert_one({"All_Brands": self.all_brands})
